2d3500c7fcca4ef4e8979dda36d3467b2f5f2d68
Igor Fedorov <bullet-1@yandex.ru>
2025-09-22 10:33:25 +0500

    KTB-04 add dictionary file parsing and output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,19 @@ import java.io.File
 fun main() {
 
     val wordsFile = File("words.txt")
+    val dictionary = mutableListOf<Word>()
 
     val wordsList = wordsFile.readLines()
     wordsList.forEach {
-        println(it)
+        val line = it.split("|")
+
+        val original = line[0]
+        val translate = line[1]
+        val correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0
+
+        val word = Word(original, translate, correctAnswersCount)
+        dictionary.add(word)
     }
+
+    println(dictionary)
 }
