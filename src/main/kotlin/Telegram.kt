6d511c3f47e2b5418edcad33ec1e4c9f03188f1a
Igor Fedorov <bullet-1@yandex.ru>
2025-10-15 17:19:31 +0500

    KTB-21 migrate to Kotlinx.Serialization

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,51 +1,35 @@
 package org.example
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import java.net.http.HttpClient
 
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    var updateId = 0
     val client: HttpClient = HttpClient.newBuilder().build()
-    val telegramBotService = TelegramBotService(botToken, client)
+    val json = Json { ignoreUnknownKeys = true }
+    val telegramBotService = TelegramBotService(botToken, client, json)
     val trainer = LearnWordsTrainer()
-
-    val updateIdRegex = "\"update_id\":(\\d+),".toRegex()
-    val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
-    val chatIdRegex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
-    val dataRegex = "\"data\":\"(.+?)\"".toRegex()
+    var updateId = 0L
 
     while (true) {
-        Thread.sleep(2000)
-        val updates = telegramBotService.getUpdates(updateId)
-        println(updates)
+        Thread.sleep(DELAY_MS)
 
-        val updateIdString = updateIdRegex
-            .findAll(updates)
-            .lastOrNull()
-            ?.groupValues
-            ?.getOrNull(1)
+        val responseString = telegramBotService.getUpdates(updateId)
+        println(responseString)
 
-        updateId = updateIdString?.toInt()?.plus(1) ?: continue
+        val response: Response = json.decodeFromString(responseString)
+        val lastUpdate = response.result.lastOrNull() ?: continue
+        updateId = lastUpdate.updateId + 1
 
-        val chatId = chatIdRegex
-            .findAll(updates)
-            .lastOrNull()
-            ?.groupValues
-            ?.getOrNull(1)
-            ?.toInt() ?: continue
+        val chatId = lastUpdate.message?.chat?.id
+            ?: lastUpdate.callbackQuery?.message?.chat?.id
+            ?: continue
 
-        val message = messageTextRegex
-            .findAll(updates)
-            .lastOrNull()
-            ?.groupValues
-            ?.getOrNull(1)
-
-        val data = dataRegex
-            .findAll(updates)
-            .lastOrNull()
-            ?.groupValues
-            ?.getOrNull(1)
+        val message = lastUpdate.message?.text
+        val data = lastUpdate.callbackQuery?.data
 
         if (message == COMMAND_START) {
             telegramBotService.sendMenu(chatId)
@@ -68,3 +52,65 @@ fun main(args: Array<String>) {
         }
     }
 }
+
+@Serializable
+data class Update(
+    @SerialName("update_id")
+    val updateId: Long,
+    @SerialName("message")
+    val message: Message? = null,
+    @SerialName("callback_query")
+    val callbackQuery: CallbackQuery? = null,
+)
+
+@Serializable
+data class Response(
+    @SerialName("result")
+    val result: List<Update>,
+)
+
+@Serializable
+data class Message(
+    @SerialName("text")
+    val text: String? = null,
+    @SerialName("chat")
+    val chat: Chat,
+)
+
+@Serializable
+data class CallbackQuery(
+    @SerialName("data")
+    val data: String,
+    @SerialName("message")
+    val message: Message,
+)
+
+@Serializable
+data class Chat(
+    @SerialName("id")
+    val id: Long,
+)
+
+@Serializable
+data class SendMessageRequest(
+    @SerialName("chat_id")
+    val chatId: Long,
+    @SerialName("text")
+    val text: String,
+    @SerialName("reply_markup")
+    val replyMarkup: ReplyMarkup? = null,
+)
+
+@Serializable
+data class ReplyMarkup(
+    @SerialName("inline_keyboard")
+    val inlineKeyboard: List<List<InlineKeyboard>>,
+)
+
+@Serializable
+data class InlineKeyboard(
+    @SerialName("text")
+    val text: String,
+    @SerialName("callback_data")
+    val callbackData: String,
+)
